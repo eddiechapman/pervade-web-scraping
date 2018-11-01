356751c9c6f620af6cadd36fbdf58afc2a9d26ff
eddiechapman <eddie.chapman4@gmail.com>
2018-10-31 20:13:56 -0500

    initial commit

---
+++ __init__.py
@@ -0,0 +1,6 @@
+from degrees import AnalyticsNCSUDegree, DataSciGradProgramsDegree
+from explorers import AnalyticsNCSUExplorer, DataSciGradProgramsExplorer
+
+if __name__ == '__main__':
+    #explorer = AnalyticsNCSUExplorer()
+    explorer = DataSciGradProgramsExplorer()
